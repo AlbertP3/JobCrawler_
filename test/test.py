3af34f31bee56c303f348cffbb27ecfe4101de85
K1C4Y <kicaj565@gmail.com>
2020-05-25 16:57:57 +0200

    małe zmiany

--- test/test.py
+++ test/test.py
@@ -75,31 +75,6 @@ class TestCase(unittest.TestCase):
         self.assertIn(b'username', response.data)
         self.assertIn(b'Password', response.data)
 
-    # def test_search_values(self):
-    #     response = self.app.get('/search', follow_redirects=True)
-    #     self.assertIn(b'keyword', response.data)
-    #     self.assertIn(b'miasta', response.data)
-    #
-    #s
-    # def test_user_creation_ok(self):
-    #     #Najczęstrze hasła 2019 wg. wikipedii + kilka polskich znaków
-    #     passwords = ['123456','123456789','qwerty','password','12\34567','12345678','12345','iloveyou','111111','123123','abc123','qwerty123','q2w3e4r','admin','qwertyuiop','654321','555555','lovely','7777777','welcome','888888','princess','dragon','password1' ]
-    #     usernames = ['Yasd69','asdw','aFERWGBTG','eabhffr234@asd']
-    #     for username in usernames:
-    #         for password in passwords:
-    #             user = User(username = username, password = password)
-    #             db.session.add(user)
-    #             db.session.commit()
-    #             self.assertEqual(user.username, username)
-    #             del user
-    #             #self.assertIn(user, db.session)
-    #
-    # def test_login_logout(client):
-    #     """Make sure login and logout works."""
-    #
-    #     rv = login(client, flaskr.app.config['USERNAME'], flaskr.app.config['PASSWORD'])
-    #     assert b'You were logged in' in rv.data
-    #
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
